6938559dbaf6cbc1dfe293f86d24240bef2ef53c
yons7 <younesyons@hotmail.com>
2015-10-30 14:37:59 -0100

    t
    t

--- server.js
+++ server.js
@@ -57,6 +57,7 @@ process.on('uncaughtException', function (err) {
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
-var server = app.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + server.address().port);
+http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
+    console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
+    server();
 });
